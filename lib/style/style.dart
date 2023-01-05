6606f8f51f9390d97a91e8fc0c94f32529ade259
donghwan <pdh980927@naver.com>
2023-01-05 17:34:15 +0900

    scaffold 틀

---
+++ lib/style/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const homeTheme =  CupertinoThemeData(
+  primaryColor: Colors.black,
+  textTheme: CupertinoTextThemeData(
+    primaryColor: Colors.black,
+  ),
+);
