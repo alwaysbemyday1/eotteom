976d3caa86e16bc22d69d74fb370b5bd51d03942
donghwan <pdh980927@naver.com>
2023-01-06 23:23:15 +0900

    틀 완성(글꼴, 크기 수정해야함)

--- lib/home/otherCodi.dart
+++ lib/home/otherCodi.dart
@@ -6,48 +6,51 @@ class OtherCodi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 600,
+      width: 350,
       height : 300,
-      padding: EdgeInsets.all(5),
+      padding: EdgeInsets.all(10),
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children:[
                   Container(
-                    padding:EdgeInsets.all(5),
-                    child:Text('다른 사람들의 코디'),
+                    padding:EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child:Text('다른 사람 코디'),
                   ),
                   Container(
-                    padding:EdgeInsets.all(5),
+                    padding:EdgeInsets.fromLTRB(0, 5, 0, 5),
                     child:Text('더보기', style: TextStyle(fontSize: 10, color: Color.fromRGBO(128,128,128, 1)),),
                   ),
                 ]
             ),
             Container(
-              padding: EdgeInsets.fromLTRB(40, 5, 0, 5),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
                     Container(
                       padding:EdgeInsets.all(2.5),
-                      child:Image.asset('assets/codi1.png', width:100, height:100),
+                      child:Image.asset('assets/codi1.png', width:130, height:130),
                     ),
                     Container(
                       padding:EdgeInsets.all(2.5),
-                      child:Image.asset('assets/codi2.png', width:100, height:100),
+                      child:Image.asset('assets/codi2.png', width:130, height:130),
                     ),
                     Container(
                       padding:EdgeInsets.all(2.5),
-                      child:Image.asset('assets/codi3.png', width:100, height:100),
+                      child:Image.asset('assets/codi3.png', width:130, height:130),
                     ),
                     Container(
                       padding:EdgeInsets.all(2.5),
-                      child:Image.asset('assets/codi4.png', width:100, height:100),
+                      child:Image.asset('assets/codi4.png', width:130, height:130),
+                    ),
+                    Container(
+                      padding:EdgeInsets.all(2.5),
+                      child:Image.asset('assets/codi5.png', width:130, height:130),
                     ),
                   ],
                 ),
