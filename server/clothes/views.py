d91122a87941a901ee9a1c8b1b03ce55298f0acc
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-01-19 18:19:00 +0900

    created clothes viewset

--- server/clothes/views.py
+++ server/clothes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Clothes
+from .serializers import ClothesSerializer
 
 # Create your views here.
+class ClothesViewSet(viewsets.ModelViewSet):
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
