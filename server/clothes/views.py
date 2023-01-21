a95b4c97fda6b7fc00c078de9d8f02d978428248
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-01-21 15:37:03 +0900

    added action for accession to user's clothes

--- server/clothes/views.py
+++ server/clothes/views.py
@@ -1,7 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
+from core.models import User
 from .models import Clothes
 from .serializers import ClothesSerializer
 
@@ -9,3 +11,14 @@ from .serializers import ClothesSerializer
 class ClothesViewSet(ModelViewSet):
     queryset = Clothes.objects.all()
     serializer_class = ClothesSerializer
+
+    @action(detail=False, methods=['get'], url_path=r'(?P<user_id>[^/.]+)')
+    def user_clothes(self, request, user_id):
+        queryset = self.get_queryset()   
+        result_queryset = queryset.filter(user=user_id)
+
+        serializer = ClothesSerializer(result_queryset, context=self.get_serializer_context(), many=True)
+        data = {
+            "results" : serializer.data
+        }
+        return Response(data)
