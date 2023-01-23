8b6ae67f45e85875347264c74c0ac311f222d18e
임태규 <kyu5787@naver.com>
2023-01-23 20:45:46 +0900

    like 구현

--- client/lib/tabs/mycloset/closet/top.dart
+++ client/lib/tabs/mycloset/closet/top.dart
@@ -1,9 +1,14 @@
 import "package:flutter/material.dart";
 import "package:sizer/sizer.dart";
 
-class Top extends StatelessWidget {
+class Top extends StatefulWidget {
   Top({super.key});
 
+  @override
+  State<Top> createState() => _TopState();
+}
+
+class _TopState extends State<Top> {
   final imagelist = [
     "assets/images/example/cloth1.png",
     "assets/images/example/cloth2.png",
@@ -12,9 +17,10 @@ class Top extends StatelessWidget {
     "assets/images/example/cloth5.png",
   ];
 
-  final imageWidth =
-      100.w - 24; // phone의 너비에서 양쪽 16px, 사진 사이 간격 8px 빼서 24px 빼주면 됨.
+  List<bool> _likedstate = [false, false, false, false, false];
 
+  final imageWidth = 100.w - 24;
+  // phone의 너비에서 양쪽 16px, 사진 사이 간격 8px 빼서 24px 빼주면 됨.
   @override
   Widget build(BuildContext context) {
     return Sizer(
@@ -32,12 +38,38 @@ class Top extends StatelessWidget {
                     crossAxisSpacing: 8),
                 itemCount: imagelist.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return ClipRRect(
-                    child: Image.asset(
-                        "assets/images/example/cloth${index + 1}.png",
-                        width: imageWidth,
-                        height: imageWidth),
-                    borderRadius: BorderRadius.circular(4.0),
+                  return Stack(
+                    children: [
+                      ClipRRect(
+                        child: Image.asset(
+                            "assets/images/example/cloth${index + 1}.png",
+                            width: imageWidth,
+                            height: imageWidth),
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          iconSize: 18,
+                          padding: const EdgeInsets.all(0),
+                          constraints: BoxConstraints(),
+                          icon: _likedstate[index]
+                              ? Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                )
+                              : Icon(
+                                  Icons.star_border_outlined,
+                                  color: Colors.black,
+                                ),
+                          onPressed: () {
+                            setState(() {
+                              _likedstate[index] = !_likedstate[index];
+                            });
+                          },
+                        ),
+                      )
+                    ],
                   );
                 }),
           ),
