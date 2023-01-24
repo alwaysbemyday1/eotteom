6a61b631c269e3b96df949a50d237a9dc34e68bb
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-01-24 23:40:20 +0900

    migrated outfits model

--- server/outfits/models.py
+++ server/outfits/models.py
@@ -8,7 +8,7 @@ from clothes.models import Clothes
 class Outfits(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, db_column="user_id", on_delete=models.CASCADE)
-    clothes = models.ManyToManyField(Clothes, blank=True, on_delete=models.CASCADE)
+    clothes = models.ManyToManyField(Clothes, blank=True)
     name = models.CharField(max_length=255)
     style = models.CharField(max_length=255, blank=True)
     date = models.DateTimeField(blank=True)
