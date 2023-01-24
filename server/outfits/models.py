62d39af2c36595040efad9d90d4ce6e8d822bffc
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-01-24 23:39:11 +0900

    created outfits model

--- server/outfits/models.py
+++ server/outfits/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+from core.models import User
+from clothes.models import Clothes
+
+class Outfits(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, db_column="user_id", on_delete=models.CASCADE)
+    clothes = models.ManyToManyField(Clothes, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    style = models.CharField(max_length=255, blank=True)
+    date = models.DateTimeField(blank=True)
+    temperature = models.IntegerField(blank=True)
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_public = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
