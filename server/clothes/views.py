bd097af06e4337f9ee05dd87552201d4783db30f
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-01-28 15:21:53 +0900

    created stats api for major category

--- server/clothes/views.py
+++ server/clothes/views.py
@@ -1,17 +1,19 @@
 from django.db.models import Count
 
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Clothes, MajorCategory
+from .models import Clothes, MajorCategory, MinorCategory
 from .serializers import ClothesSerializer, ClothesRetrieveSerializer
 
 # Create your views here.
 class ClothesViewSet(ModelViewSet):
     queryset = Clothes.objects.all()
-    category_queryset = MajorCategory.objects.all()
+    majorcategory_queryset = MajorCategory.objects.all()
+    minorcategory_queryset = MinorCategory.objects.all()
     serializer_class = ClothesSerializer
 
     def list(self, request, *args, **kwargs):
@@ -43,13 +45,14 @@ class ClothesViewSet(ModelViewSet):
         queryset = self.get_queryset().filter(user=user_id)
         
         total_count = queryset.count()
-        category_count = [] 
-        category_value = queryset.values('major_category').annotate(count=Count('major_category'))
-        for i in category_value:
-            new_category_value = {'major_category': self.category_queryset.filter(id=1).values('name_en').get()['name_en'], 'count':i['count']}
-            category_count.append(new_category_value)
         color_count = queryset.values('color').annotate(count=Count('color'))
         brand_count = queryset.values('brand').annotate(count=Count('brand'))
+
+        category_count = []
+        category_value = queryset.values('major_category').annotate(count=Count('major_category'))
+        for i in category_value:
+            new_category_value = {'major_category': self.majorcategory_queryset.filter(id=1).values('name_en').get()['name_en'], 'count':i['count']}
+            category_count.append(new_category_value)
 
         data = {
             "total_count" : total_count,
@@ -58,4 +61,32 @@ class ClothesViewSet(ModelViewSet):
             "brand_count" : brand_count,
         }
         return Response(data)
+
+        
+    @action(detail=False, methods=['get'], url_path=r'stats/(?P<user_id>[^/.]+)/(?P<major_category>[a-z]+)')
+    def user_clothes_stats_major(self, request, user_id, major_category):
+        majorcategory_list = [i['name_en'] for i in self.majorcategory_queryset.values()]
+        if major_category not in majorcategory_list:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            queryset = self.get_queryset().filter(user=user_id,
+                major_category=self.majorcategory_queryset.filter(name_en=major_category).values('id').get()['id'])
+            
+            total_count = queryset.count()
+            color_count = queryset.values('color').annotate(count=Count('color'))
+            brand_count = queryset.values('brand').annotate(count=Count('brand'))
+
+            category_count = []
+            category_value = queryset.values('minor_category').annotate(count=Count('minor_category'))
+            for i in category_value:
+                new_category_value = {'minor_category': self.minorcategory_queryset.filter(id=i['minor_category']).values('name_en').get()['name_en'], 'count':i['count']}
+                category_count.append(new_category_value)
+                
+            data = {
+                "total_count" : total_count,
+                "category_count" : category_count,
+                "color_count" : color_count,
+                "brand_count" : brand_count,
+            }
+            return Response(data)
 
