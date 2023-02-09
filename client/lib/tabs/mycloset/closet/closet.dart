87b00a918edc2eb51d26b5b9cb4118f3ae7a190e
임태규 <kyu5787@naver.com>
2023-02-09 10:29:21 +0900

    button layout 통일

--- client/lib/tabs/mycloset/closet/closet.dart
+++ client/lib/tabs/mycloset/closet/closet.dart
@@ -6,6 +6,7 @@ import "package:eotteom/provider.dart";
 import 'package:flutter/material.dart';
 import "package:provider/provider.dart";
 import "package:eotteom/tabs/mycloset/enrollclothes/enroll.dart";
+import "package:sizer/sizer.dart";
 
 
 class Closet extends StatefulWidget {
@@ -20,59 +21,53 @@ class _ClosetState extends State<Closet> {
   Widget build(BuildContext context) {
     var categories = context.watch<ClosetProvider>().totalMap.keys.toList();
     var categoryPressed = context.watch<ClosetProvider>().categoryPress;
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 18),
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: DropDownCloset(),
-          ),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return SizedBox(
+          height: 200.h,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 18),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: DropDownCloset(),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: context.read<ClosetProvider>().categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: TextButton(
+                        child: Text(
+                          categories[index],
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xff151515),
+                              decoration: categoryPressed[index]
+                                  ? TextDecoration.underline
+                                  : TextDecoration.none,
+                              decorationThickness: 2,
+                              fontSize: 16,
+                              fontFamily: "NotoSans"),
+                        ),
+                        onPressed: () {
+                          context.read<ClosetProvider>().selectFirstIndex(index);
+                        },
+                      ),
+                    );
+                  }),
+            ),
+            SelectCategory(),
+            ClothRender(),
+          ],
         ),
-        SizedBox(
-          height: 40,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: context.read<ClosetProvider>().categories.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(left: 8, right: 8),
-                  child: TextButton(
-                    child: Text(
-                      categories[index],
-                      style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xff151515),
-                          decoration: categoryPressed[index]
-                              ? TextDecoration.underline
-                              : TextDecoration.none,
-                          decorationThickness: 2,
-                          fontSize: 16,
-                          fontFamily: "NotoSans"),
-                    ),
-                    onPressed: () {
-                      context.read<ClosetProvider>().selectFirstIndex(index);
-                    },
-                  ),
-                );
-              }),
-        ),
-        SelectCategory(),
-        ClothRender(),
-        Padding(
-          padding: const EdgeInsets.only(right: 16),
-          child: Align(
-            child: FloatingActionButton(
-              backgroundColor: Color(0xff151515),
-                onPressed: () {
-                  Navigator.of(context, rootNavigator: true).push(
-                      CupertinoPageRoute(builder: (ctx) => ClothesEnroll()));
-                },
-                child: Icon(Icons.add)),
-            alignment: Alignment.centerRight,
-          ),
-        ),
-      ],
+      );
+      },
     );
   }
 }
--- client/lib/tabs/mycloset/mycloset.dart
+++ client/lib/tabs/mycloset/mycloset.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import 'closet/closet_appbar.dart';
 import 'closet/closet.dart';
 import "package:eotteom/provider.dart";
+import "package:eotteom/tabs/mycloset/enrollclothes/enroll.dart";
+import 'package:flutter/cupertino.dart';
 
 class MyCloset extends StatelessWidget {
   const MyCloset({super.key});
@@ -15,6 +17,16 @@ class MyCloset extends StatelessWidget {
       ],
       child: MaterialApp(
         home: Scaffold(
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(right: 16, bottom: 40),
+            child: FloatingActionButton(
+                backgroundColor: Color(0xff151515),
+                onPressed: () {
+                  Navigator.of(context, rootNavigator: true).push(
+                      CupertinoPageRoute(builder: (ctx) => ClothesEnroll()));
+                },
+                child: Icon(Icons.add)),
+          ),
           appBar: appbar,
           body: Closet(),
         ),
