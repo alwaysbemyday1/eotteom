0dabb80b98bf529edcf19a047e4835ceb55620f8
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-02-16 16:07:01 +0900

    modified outfit model

--- server/outfits/models.py
+++ server/outfits/models.py
@@ -10,7 +10,7 @@ class Outfit(models.Model):
     STYLE_CHOICES = (('캐주얼', '캐주얼'),('클래식', '클래식'),('빈티지', '빈티지'),('스트릿', '스트릿'),('스포티', '스포티'),('모던', '모던'),('댄디', '댄디'),('미니멀', '미니멀'),('레이어드', '레이어드'),('논코어', '논코어'),('페미닌', '페미닌'),('맥시멀', '맥시멀'),('아메리칸 캐주얼', '아메리칸 캐주얼'),('에스닉', '에스닉'),('아방가르드', '아방가르드'))
     
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, db_column="user_id", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, db_column="user_id", on_delete=models.CASCADE, related_name='outfit')
     name = models.CharField(max_length=255)
     image = models.ImageField()
     clothes = models.ManyToManyField(Clothes, blank=True, related_name='in_outfit')
